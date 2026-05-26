920d6e3140829117800831a4846037fc4b1b1fb6
Ashwin M <ashwinnambiarm@gmail.com>
2026-05-26 23:57:55 +0400

    added basic files

---
+++ app/core/app.route.js
@@ -0,0 +1,6 @@
+angular.module("YTS").config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/ui/main.html"
+    });
+});
---
+++ app/main.js
@@ -0,0 +1,2 @@
+import './core/app.module.js';
+import './core/app.route.js';
